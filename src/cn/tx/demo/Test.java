e75ca9223660e4a2d3a82692946e1c9acd8ed0ee
lokshi <gzlshi@163.com>
2020-10-22 16:27:10 +0800

    Java 拓薪教程 腾讯课堂

--- src/cn/tx/demo/Test.java
+++ src/cn/tx/demo/Test.java
@@ -2,9 +2,35 @@ package cn.tx.demo;
 
 public class Test {
     public static void main(String[] args) {
-        int a =5;
-        boolean result = (a++>5) && (++a >4);
-        System.out.println(result);
-        System.out.println(a);
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <=(5-i) ; j++) {
+                System.out.print(" ");
+                }
+            for (int a = 1; a <=2*i-1 ; a++) {
+                //打印空心菱形
+                if(a==1 || a ==(2*i-1)){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+
+            }
+        System.out.println();
+        }
+
+        for (int i = 4; i > 0; i--) {
+            for (int j = 1; j <=(5-i) ; j++) {
+                System.out.print(" ");
+            }
+            for (int a = 1; a <=2*i-1 ; a++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+
     }
+
 }
+
