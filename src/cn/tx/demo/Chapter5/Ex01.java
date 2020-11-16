69c4e1cf0daa7f2e94ee3ae16f63433df9fb7a66
lokshi <gzlshi@163.com>
2020-11-16 17:28:38 +0800

    Java 拓薪教程 腾讯课堂

---
+++ src/cn/tx/demo/Chapter5/Ex01.java
@@ -0,0 +1,50 @@
+package cn.tx.demo.Chapter5;
+
+import java.util.Random;
+
+class A {
+    int v=9;
+}
+
+class Guess{
+    int a;
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public Guess(int a){
+        this.a=a;
+    }
+    public  Guess(){
+
+    }
+    public boolean  dd(){
+        boolean flag=false;
+        Random d = new Random();
+//        System.out.println(d.nextInt(10));
+
+        A guess = new A();
+        if (a ==guess.v){
+            //System.out.println("Yes");
+            flag=true;
+        }else{
+            //System.out.println("Worng");
+            flag=false;
+        }
+        return flag;
+    }
+}
+
+public class Ex01 {
+    public static void main(String[] args) {
+        Guess guess = new Guess();
+        guess.setA(6);
+        boolean flag =guess.dd();
+        if(flag){
+            System.out.println("yes");
+        }else {
+            System.out.println("fail");
+        }
+    }
+}
