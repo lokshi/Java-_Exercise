15c7259202bb5d2f00371bfbd71e5d609f90659d
gzlshi <gzlshi@163.com>
2020-11-19 22:42:58 +0800

    ubuntu20

---
+++ src/cn/tx/demo/Chapter5/Ex06.java
@@ -0,0 +1,42 @@
+package src.cn.tx.demo.Chapter5;
+
+class MyNumber {
+    private int n1;
+    private int n2;
+
+    public MyNumber(int n1, int n2) {
+        this.n1 = n1;
+        this.n2 = n2;
+    }
+
+
+    public void addition() {
+        int add = n1 + n2;
+        System.out.printf("%s + %s = %s \n", n1, n2, add);
+    }
+
+    public void subtration() {
+        int sub = n1 - n2;
+        System.out.printf("%s - %s = %s \n", n1, n2, sub);
+    }
+
+    public void multiplication() {
+        int mul = n1 * n2;
+        System.out.printf("%s * %s = %s \n", n1, n2, mul);
+    }
+
+    public void division() {
+        int div = n1 / n2;
+        System.out.printf("%s / %s = %s \n", n1, n2, div);
+    }
+
+}
+
+
+public class Ex06 {
+        MyNumber match = new MyNumber(20,15);
+        match.addition();
+        match.subtration();
+        match.multiplication();
+        match.division();
+}
