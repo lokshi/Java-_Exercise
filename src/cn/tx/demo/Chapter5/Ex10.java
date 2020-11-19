9aa03e20ca47f0e9b6d9bea8d97d8e5d9f59a8ba
gzlshi <gzlshi@163.com>
2020-11-20 00:53:30 +0800

    ubuntu20

---
+++ src/cn/tx/demo/Chapter5/Ex10.java
@@ -0,0 +1,16 @@
+package src.cn.tx.demo.Chapter5;
+
+class WuMingFen{
+    String theMa;
+    int quantity;
+    boolean likeSup;
+
+   public
+
+}
+
+public class Ex10 {
+    public static void main(String[] args) {
+
+    }
+}
